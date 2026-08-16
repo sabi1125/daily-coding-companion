68fcc4cee3f51f13a8e6acdccc0a823b426f7c96
sabi1125 <sabirbarahi41@gmail.com>
2026-08-17 02:28:52 +0900

    add: added ingest engine

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+
+	"backend/cmd/ingest"
 	"backend/internal/config"
 	"backend/internal/infrastructure"
 	"backend/internal/log"
@@ -14,11 +17,25 @@ func main() {
 	zapCfg := config.LoadZapConfig()
 	logger.Init(zapCfg)
 	defer logger.Sync()
-
 	validator.Init()
 
 	dbCfg := config.LoadDbConfig()
 	db := infrastructure.Connection(dbCfg)
+
+	args := os.Args
+
+	if len(args) == 2 {
+		if args[1] == "ingest" {
+			ingest.Ingest(db)
+			return
+		} else {
+			logger.Fatal("invalid argument")
+			return
+		}
+	} else if len(args) > 1 {
+		logger.Fatal("invalid number of arguments")
+		return
+	}
 
 	// Create an Echo instance
 	e := echo.New()
