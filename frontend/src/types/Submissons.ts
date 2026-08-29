b356fe7a3b0e07a640d22453b2b9be564784c3ff
sabi1125 <sabirbarahi41@gmail.com>
2026-08-29 18:24:13 +0900

    add: problem execution result component added to frontend

--- frontend/src/types/Submissons.ts
+++ frontend/src/types/Submissons.ts
@@ -15,3 +15,27 @@ export interface SubmissionHistory {
   result: Submission[]
   total: number
 }
+
+export interface ExecuteSubmittedSolution {
+  language: string
+  content: string
+}
+
+export interface ExecuteResposne {
+  language: string
+  version: string
+  run: Run
+}
+
+export interface Run {
+  stdout: string
+  stderr: string
+  output: string
+  code: number
+  signal: string | null
+  cpu_time: number
+  wall_time: number
+  memory: number
+  message: string | null
+  status: string | null
+}
